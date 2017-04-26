1071af0c88cac0a0ecaced780046c1fd8da5e434
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-26 20:56:00 +0200

    Move comment to top of class

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+/**
+  * Ian parses JSON
+  * @param {String} json some JSON for Ian to parse
+  * @return {Object} the JSON, parsed by Ian
+  */
 class Ian {
-  /**
-   * Ian parses JSON
-   * @param {String} json some JSON for Ian to parse
-   * @return {Object} the JSON, parsed by Ian
-   */
   parse(json) {
     return JSON.parse(json);
   }
